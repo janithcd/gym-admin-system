6b2a02172d4f75094b056071c4a9dc0407d20610
janithcd <djanith11@gmail.com>
2026-06-27 17:26:57 +0530

    First Commit

---
+++ src/main/java/lk/janith/gymadmin/GymAdminSystemApplication.java
@@ -0,0 +1,13 @@
+package lk.janith.gymadmin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GymAdminSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GymAdminSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/lk/janith/gymadmin/GymAdminSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package lk.janith.gymadmin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GymAdminSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
